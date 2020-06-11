5a3a354a8600830e43456cde270e0e6f6a0b5dbe
anton-kn <antonknjzev@yandex.ru>
2020-06-11 06:52:29 +0400

    add arrow for mobile version

--- public/js/arrow.js
+++ public/js/arrow.js
@@ -1,14 +1,11 @@
 const cover = document.querySelector('.cover');
 const arrow = document.querySelector('.arrow');
 
-cover.addEventListener('mousemove',(event)=>{
-	//console.log(event);
-	console.log(window.pageYOffset);
+cover.addEventListener('mouseover',(event)=>{
+	// console.log(event);
 	if(window.pageYOffset > 300){
-		arrow.style.display = 'block';
 		arrow.className = 'arrow showarrow';
 	}else{
-		arrow.style.display = 'none';
 		arrow.className = 'arrow';
 	}
 });
@@ -16,5 +13,5 @@ cover.addEventListener('mousemove',(event)=>{
 
 arrow.addEventListener('click', ()=>{
 	window.scrollTo(0,0);
-	arrow.style.display = 'none';
+	arrow.className = 'arrow';
 });
